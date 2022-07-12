ae3960a23ee9ffa2467024f47989bf73e9662b75
Rachely-Braverman <rachelb55677@gmail.com>
2022-07-12 22:02:21 +0300

    test for users on swagger

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const user = require('./routes/userRouter');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+// app.use(bodyParser);
+// app.use(bodyParser.urlencoded({
+// 	extended: true
+// }));
 app.use(bodyParser.json());
 app.use(cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/users', user);
 
