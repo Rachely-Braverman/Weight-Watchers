bd5a7e0dafb161ff207f02dbbc986246557e552b
Rachely-Braverman <rachelb55677@gmail.com>
2022-07-12 22:11:10 +0300

    BasicCode

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-const user = require('./routes/userRouter');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 app.use(cors())
-
-app.use('/users', user);
 
 const port=process.env.PORT || 3000;
 app.listen(port, ()=> {
